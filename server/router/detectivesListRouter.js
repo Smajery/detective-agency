1e2babebeccd48e93d187a6d030e2fbb18f04551
Smajery <vasiliew.sasha@gmail.com>
2023-05-31 17:41:34 +0200

    deleted useless routes

--- server/router/detectivesListRouter.js
+++ server/router/detectivesListRouter.js
@@ -3,8 +3,5 @@ const router = new Router()
 const detectivesListController = require('../controllers/detectivesListController')
 
 router.post('/', detectivesListController.create)
-router.get('/', detectivesListController.getAll)
-router.get('/:id', detectivesListController.getOne)
-router.delete('/:id', detectivesListController.delete)
 
 module.exports = router
